b7325a0f484ffd93471dd6cac990642ff168f32e
Val Pal <valgonzalez13@yahoo.com>
2022-10-04 00:23:29 -0500

    re-structuring routes

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('./public/assets/js/index');
 
 const PORT = 3001;
 
 const app = express();
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', api);
-
-app.use(express.static('public'));
-
-// GET Route for main page
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-});
-
-// GET Route for notes
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 //Listen to server and show it is running
 app.listen(PORT, () => console.log(`App listening at http://localhost: ${PORT} 🚀`));
