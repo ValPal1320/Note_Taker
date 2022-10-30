25184f3218511a570fc3b7e5b1b1792febb41bbd
Val Pal <valgonzalez13@yahoo.com>
2022-10-30 17:28:02 -0500

    cleaned up and added comments

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+// Require dependencies
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
 const PORT = 3001;
-
 const app = express();
 
 const apiRoutes = require('./routes/apiRoutes');
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//Require routes file
+require('./routes/routes')(app);
 
 //Listen to server and show it is running
 app.listen(PORT, () => console.log(`App listening at http://localhost: ${PORT} 🚀`));
