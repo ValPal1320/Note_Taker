1f344e18ad167317ca612690393de26e3bf4a656
Val Pal <valgonzalez13@yahoo.com>
2022-11-08 22:01:08 -0600

    saving and posting new note now working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Require dependencies
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json');
 const fs = require('fs');
 
 // Initialize express
@@ -9,11 +10,31 @@ const app = express();
 
 // Data parsing
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-//Require routes file
-require('./routes/routes')(app);
+// GET files for path joining
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+});
+
+// GET any saved notes from db file
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+});
+
+app.post('/api/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    notes.push(newNote)
+    res.json('New note added successfully!')
+});
 
 //Listen to server and show it is running
 app.listen(PORT, () => console.log(`App listening at http://localhost: ${PORT} 🚀`));
